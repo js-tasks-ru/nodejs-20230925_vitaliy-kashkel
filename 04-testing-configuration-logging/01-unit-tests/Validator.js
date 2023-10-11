165bb0d4311a101a923a734dc4294f5f518baf3c
Vitaly <vitaliy@uxpressia.com>
2023-10-12 00:55:36 +0300

    VK. Homework 04-01

--- 04-testing-configuration-logging/01-unit-tests/Validator.js
+++ 04-testing-configuration-logging/01-unit-tests/Validator.js
@@ -1,5 +1,41 @@
+/* eslint-disable max-len */
+function validateEnterRules(rules) {
+  if (!rules || typeof rules !== 'object' || Object.values(rules).length === 0) {
+    throw new Error('Set validation rules.');
+  }
+
+  Object.values(rules).forEach((rule) => {
+    const ruleKeys = Object.keys(rule);
+    if (!ruleKeys.includes('type')) {
+      throw Error('The "type" field in the validation rule is required.');
+    }
+
+    if (!['string', 'number'].includes(rule.type)) {
+      throw Error(`The value of the "type" field must be "string" or "number" and not "${rule.type}".`);
+    }
+
+    if (!ruleKeys.includes('min')) {
+      throw Error('The "min" field in the validation rule is required.');
+    }
+
+    if (typeof rule.min !== 'number') {
+      throw Error(`The value type of the "min" field must be "number" and not "${typeof rule.min}".`);
+    }
+
+    if (!ruleKeys.includes('max')) {
+      throw Error('The "max" field in the validation rule is required.');
+    }
+
+    if (typeof rule.max !== 'number') {
+      throw Error(`The value type of the "max" field must be "number" and not "${typeof rule.max}".`);
+    }
+  });
+}
+
+
 module.exports = class Validator {
   constructor(rules) {
+    validateEnterRules(rules);
     this.rules = rules;
   }
 
@@ -31,7 +67,7 @@ module.exports = class Validator {
             errors.push({field, error: `too little, expect ${rules.min}, got ${value}`});
           }
           if (value > rules.max) {
-            errors.push({field, error: `too big, expect ${rules.min}, got ${value}`});
+            errors.push({field, error: `too big, expect ${rules.max}, got ${value}`});
           }
           break;
       }
--- 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
+++ 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
@@ -1,22 +1,183 @@
+/* eslint-disable max-len */
 const Validator = require('../Validator');
 const expect = require('chai').expect;
 
-describe('testing-configuration-logging/unit-tests', () => {
+describe.only('testing-configuration-logging/unit-tests', () => {
   describe('Validator', () => {
-    it('валидатор проверяет строковые поля', () => {
+    it('невозможно создание валидатора если не задать правила валидации', () => {
+      try {
+        // eslint-disable-next-line no-unused-vars
+        const validator = new Validator();
+      } catch (e) {
+        expect(e.message).to.be.equal('Set validation rules.');
+      }
+    });
+
+    it('невозможно создание валидатора если в правиле валидации отсутствует поле "type"', () => {
+      try {
+        // eslint-disable-next-line no-unused-vars
+        const validator = new Validator({
+          name: {
+            min: 5,
+            max: 20,
+          },
+        });
+      } catch (e) {
+        expect(e.message).to.be.equal('The "type" field in the validation rule is required.');
+      }
+    });
+
+    it('невозможно создание валидатора если в правиле валидации значение поля "type" не "string" или "number" (например "array")', () => {
+      try {
+        // eslint-disable-next-line no-unused-vars
+        const validator = new Validator({
+          name: {
+            type: 'array',
+            min: 5,
+            max: 20,
+          },
+        });
+      } catch (e) {
+        expect(e.message).to.be.equal('The value of the "type" field must be "string" or "number" and not "array".');
+      }
+    });
+
+    it('невозможно создание валидатора если в правиле валидации отсутствует поле "min"', () => {
+      try {
+        // eslint-disable-next-line no-unused-vars
+        const validator = new Validator({
+          name: {
+            type: 'string',
+            max: 20,
+          },
+        });
+      } catch (e) {
+        expect(e.message).to.be.equal('The "min" field in the validation rule is required.');
+      }
+    });
+
+    it('невозможно создание валидатора если в правиле валидации тип значения поля "min" не "number" (например "string")', () => {
+      try {
+        // eslint-disable-next-line no-unused-vars
+        const validator = new Validator({
+          name: {
+            type: 'string',
+            min: '5',
+            max: 20,
+          },
+        });
+      } catch (e) {
+        expect(e.message).to.be.equal('The value type of the "min" field must be "number" and not "string".');
+      }
+    });
+
+    it('невозможно создание валидатора если в правиле валидации отсутствует поле "max"', () => {
+      try {
+        // eslint-disable-next-line no-unused-vars
+        const validator = new Validator({
+          name: {
+            type: 'string',
+            min: 5,
+          },
+        });
+      } catch (e) {
+        expect(e.message).to.be.equal('The "max" field in the validation rule is required.');
+      }
+    });
+
+    it('невозможно создание валидатора если в правиле валидации тип значения поля "max" не "number" (например "boolean")', () => {
+      try {
+        // eslint-disable-next-line no-unused-vars
+        const validator = new Validator({
+          name: {
+            type: 'string',
+            min: 5,
+            max: true,
+          },
+        });
+      } catch (e) {
+        expect(e.message).to.be.equal('The value type of the "max" field must be "number" and not "boolean".');
+      }
+    });
+
+    it('валидатор проверяет что длина значения поля строкового типа не меньше значения поля "min"', () => {
       const validator = new Validator({
         name: {
           type: 'string',
-          min: 10,
+          min: 5,
           max: 20,
         },
       });
 
-      const errors = validator.validate({ name: 'Lalala' });
+      const errors = validator.validate({name: 'La'});
 
       expect(errors).to.have.length(1);
       expect(errors[0]).to.have.property('field').and.to.be.equal('name');
-      expect(errors[0]).to.have.property('error').and.to.be.equal('too short, expect 10, got 6');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('too short, expect 5, got 2');
+    });
+
+    it('валидатор проверяет что длина значения поля строкового типа не больше значения поля "max"', () => {
+      const validator = new Validator({
+        name: {
+          type: 'string',
+          min: 5,
+          max: 10,
+        },
+      });
+
+      const errors = validator.validate({name: 'Lalalalalala'});
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('name');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('too long, expect 10, got 12');
+    });
+
+    it('валидатор проверяет что значение поля числового типа не меньше значения поля "min"', () => {
+      const validator = new Validator({
+        age: {
+          type: 'number',
+          min: 5,
+          max: 20,
+        },
+      });
+
+      const errors = validator.validate({age: 2});
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('age');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('too little, expect 5, got 2');
+    });
+
+    it('валидатор проверяет что значение поля числового типа не больше значения поля "max"', () => {
+      const validator = new Validator({
+        age: {
+          type: 'number',
+          min: 5,
+          max: 20,
+        },
+      });
+
+      const errors = validator.validate({age: 29});
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('age');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('too big, expect 20, got 29');
+    });
+
+    it('валидатор проверяет что значени поля числового типа не больше значения поля "max"', () => {
+      const validator = new Validator({
+        age: {
+          type: 'number',
+          min: 5,
+          max: 20,
+        },
+      });
+
+      const errors = validator.validate({age: 29});
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('age');
+      expect(errors[0]).to.have.property('error').and.to.be.equal('too big, expect 20, got 29');
     });
   });
 });
